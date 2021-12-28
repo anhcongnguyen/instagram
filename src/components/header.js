3f086fbaf908c48d5f66fcf131c972802a18d119
Nguyen Cong Anh <nguyenanhyoung@gmail.com>
2021-12-29 00:37:53 +0700

    header inprogress 80%

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React, { useContext } from 'react';
 import FirebaseContext from '../context/firebase';
 import UserContext from '../context/user';
 import * as ROUTES from '../constants/routes';
-import { BrowserRouter as Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     const { firebase } = useContext(FirebaseContext);
@@ -15,16 +15,16 @@ export default function Header() {
                 <div className="flex justify-between h-full">
                     <div className="text-gray-700 text-center flex items-center aligm-items cursor-pointer">
                         <h1 className="flex justify-center w-full">
-                            <Route path={ROUTES.DASHBOARD} aria-label="Instagram logo">
+                            <Link to={ROUTES.DASHBOARD} aria-label="Instagram logo">
                                 <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12" />
-                            </Route>
+                            </Link>
                         </h1>
                     </div>
                     <div className="text-gray-700 text-center flex items-center align-items">
                         {
                             user ? (
                                 <>
-                                    <Route path={ROUTES.DASHBOARD} aria-label="Dashboard">
+                                    <Link to={ROUTES.DASHBOARD} aria-label="Dashboard">
                                         <svg
                                             className="w-8 mr-6 text-black-light cursor-pointer"
                                             xmlns="http://www.w3.org/2000/svg"
@@ -39,7 +39,7 @@ export default function Header() {
                                                 d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
                                             />
                                         </svg>
-                                    </Route>
+                                    </Link>
 
                                     <button
                                         type="button"
@@ -66,17 +66,23 @@ export default function Header() {
                                         </svg>
                                     </button>
                                     <div className="flex items-center cursor-pointer">
-                                        <Route path={`/p/${user.displayName}`}>
+                                        <Link to={`/p/${user.displayName}`}>
                                             <img
                                                 className='rounded-full h-8 w-8 flex'
                                                 src={`/images/avatars/${user.displayName}.jpg`}
                                                 alt={`${user.displayName} profile`}
                                             />
-                                        </Route>
+                                        </Link>
                                     </div>
                                 </>
                             ) : (
                                 <>
+                                    <Link to={ROUTES.LOGIN}>
+                                        <button type='button' className='bg-blue-medium font-bold text-sm rounded text-white w-20 h-8'>Log In</button>
+                                    </Link>
+                                    <Link to={ROUTES.SIGN_UP}>
+                                        <button type='button' className='font-bold text-sm rounded text-blue-medium w-20 h-8'>Sign Up</button>
+                                    </Link>
                                 </>
                             )
                         }
